dd63ba636d20c470bec5870ae184de1a87a8bdcf
gabrielrov <gabrielrov.c@gmail.com>
2026-04-15 10:41:26 -0300

    feat(nvim): add cmds to lazyload lazygit.nvim

--- nvim/.config/nvim/lua/plugins/lazygit.lua
+++ nvim/.config/nvim/lua/plugins/lazygit.lua
@@ -3,6 +3,7 @@ return {
   dependencies = {
     'nvim-lua/plenary.nvim',
   },
+  cmd = { 'LazyGit', 'LazyGitCurrentFile', 'LazyGitFilter', 'LazyGitFilterCurrentFile' },
   keys = {
     { '<leader>l', '<cmd>LazyGit<CR>', desc = 'LazyGit' },
   },
