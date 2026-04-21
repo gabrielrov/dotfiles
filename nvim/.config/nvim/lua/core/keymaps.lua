7e2b435e01bf4221ec89b15ff1de070944b4476e
gabrielrov <gabrielrov.c@gmail.com>
2026-04-21 18:25:51 -0300

    feat(nvim): add new key to go to previous tab

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -183,6 +183,7 @@ vim.keymap.set({ 'n', 'x' }, '<C-w>O', '<cmd>tabo<CR>', { desc = 'Tab only' })
 vim.keymap.set({ 'n', 'x' }, '<C-w>C', '<cmd>tabclose<CR>', { desc = 'Tab close' })
 vim.keymap.set('n', "g'", '<cmd>tabn<CR>')
 vim.keymap.set('n', 'g"', '<cmd>tabp<CR>')
+vim.keymap.set('n', 'g<Space>', 'g<Tab>')
 
 vim.keymap.set({ 'n', 'x' }, 'g1', '<cmd>tabn 1<CR>', { desc = 'Go to tab n1' })
 vim.keymap.set({ 'n', 'x' }, 'g2', '<cmd>tabn 2<CR>', { desc = 'Go to tab n2' })
