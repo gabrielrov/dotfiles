44b9ff0f970dc7994f32270319a7b3576a32d2fc
gabrielrov <gabrielrov.c@gmail.com>
2026-04-24 20:02:25 -0300

    feat(nvim): change dap prompts format

--- nvim/.config/nvim/lua/custom/nvim-dap/pick_dap_port.lua
+++ nvim/.config/nvim/lua/custom/nvim-dap/pick_dap_port.lua
@@ -3,7 +3,7 @@ return function(default)
 
   local co = coroutine.running()
   return coroutine.create(function()
-    vim.ui.input({ prompt = 'Attach on port', default = default }, function(port)
+    vim.ui.input({ prompt = 'Attach on port: ', default = default }, function(port)
       if port == nil or port == '' then
         return
       end
