0714068e9bbfd36e91ecde3505b844a4a8595edc
gabrielrov <gabrielrov.c@gmail.com>
2026-04-24 19:35:19 -0300

    feat(tmux): harness of new sesh list -b flag
    Bind keys to it and use on cleanup script

--- tmux/.tmux_cleanup_sessions.sh
+++ tmux/.tmux_cleanup_sessions.sh
@@ -1,15 +1,13 @@
 #!/usr/bin/env bash
 
-REGEX=$(tmux show-option -gv @cleanup_regex 2>/dev/null)
-
-[[ -z "$REGEX" ]] && exit 0
-
 sessions=$(tmux list-sessions -F '#{session_name}' 2>/dev/null) || exit 0
-
 [[ -z "$sessions" ]] && exit 0
 
-while IFS= read -r name; do
-  if [[ ! "$name" =~ $REGEX ]]; then
+blacklisted=$(sesh list -bt 2>/dev/null)
+[[ -z "$blacklisted" ]] && exit 0
+
+while IFS= read -r session; do
+  if ! grep -qxF "$session" <<< "$blacklisted"; then
     exit 0
   fi
 done <<< "$sessions"
