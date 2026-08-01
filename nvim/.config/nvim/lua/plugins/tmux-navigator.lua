1c033b2b0cc4911f3daeff036012de87f061b157
gabrielrov <gabrielrov.c@gmail.com>
2026-07-31 22:01:05 -0300

    refactor(nvim): handle repetitive binding mode definitions

--- nvim/.config/nvim/lua/plugins/tmux-navigator.lua
+++ nvim/.config/nvim/lua/plugins/tmux-navigator.lua
@@ -5,9 +5,11 @@ return {
     vim.g.tmux_navigator_no_mappings = 1
   end,
   config = function()
-    vim.keymap.set({ 'n', 'i', 'x', 's', 'o', 't', 'c' }, '<A-h>', '<cmd>TmuxNavigateLeft<cr>')
-    vim.keymap.set({ 'n', 'i', 'x', 's', 'o', 't', 'c' }, '<A-j>', '<cmd>TmuxNavigateDown<cr>')
-    vim.keymap.set({ 'n', 'i', 'x', 's', 'o', 't', 'c' }, '<A-k>', '<cmd>TmuxNavigateUp<cr>')
-    vim.keymap.set({ 'n', 'i', 'x', 's', 'o', 't', 'c' }, '<A-l>', '<cmd>TmuxNavigateRight<cr>')
+    local modes = { 'n', 'i', 'x', 's', 'o', 't', 'c' }
+
+    vim.keymap.set(modes, '<A-h>', '<cmd>TmuxNavigateLeft<cr>')
+    vim.keymap.set(modes, '<A-j>', '<cmd>TmuxNavigateDown<cr>')
+    vim.keymap.set(modes, '<A-k>', '<cmd>TmuxNavigateUp<cr>')
+    vim.keymap.set(modes, '<A-l>', '<cmd>TmuxNavigateRight<cr>')
   end,
 }
