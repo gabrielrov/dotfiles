5ae806ea860b258f1798e63777a604f5a144d637
gabrielrov <gabrielrov.c@gmail.com>
2026-08-01 22:33:05 -0300

    feat(yazi): hide "Loading..." message on statup

--- yazi/.config/yazi/init.lua
+++ yazi/.config/yazi/init.lua
@@ -1,3 +1,12 @@
+local old_empty = Current.empty
+Current.empty = function(self, ...)
+	if not self._folder.stage() then
+		return {} -- hide "Loading..."
+	else
+		return old_empty(self, ...)
+	end
+end
+
 require("bookmarks"):setup({
 	persist = "all",
 	file_pick_mode = "parent",
