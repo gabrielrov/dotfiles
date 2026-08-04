970c8129e83c713ce47ea99dcccc78ccf9a9858a
gabrielrov <gabrielrov.c@gmail.com>
2026-08-04 14:05:58 -0300

    refactor(nvim): merge colorscheme and themery configs

--- nvim/.config/nvim/lua/plugins/colorschemes.lua
+++ nvim/.config/nvim/lua/plugins/colorschemes.lua
@@ -1,26 +1,40 @@
 return {
+  { 'EdenEast/nightfox.nvim', lazy = true },
+  { 'catppuccin/nvim', name = 'catppuccin', lazy = true },
+  { 'folke/tokyonight.nvim', lazy = true },
+  { 'projekt0n/github-nvim-theme', lazy = true },
   {
-    'EdenEast/nightfox.nvim',
-    lazy = true,
-  },
-  {
-    'catppuccin/nvim',
-    name = 'catppuccin',
-    lazy = true,
+    'zaldih/themery.nvim',
+    lazy = false,
+    keys = {
+      { '<leader>t', '<cmd>Themery<cr>', desc = 'Themery' },
+    },
     config = function()
-      require('catppuccin').setup({
-        integrations = {
-          render_markdown = false,
+      vim.cmd('colorscheme duskfox') -- default colorscheme
+
+      require('utils.ft').clear_c_hjkl('themery', { bind_c_j = true })
+      require('utils.ft').bind_tmux_nav('themery')
+
+      require('themery').setup({
+        themes = {
+          'duskfox',
+          'nordfox',
+          'nightfox',
+
+          'catppuccin-mocha',
+          'catppuccin-macchiato',
+          'catppuccin-frappe',
+
+          'tokyonight-moon',
+          'tokyonight-night',
+          'tokyonight-storm',
+
+          'github_dark_dimmed',
+          'carbonfox',
+          'github_dark_default',
         },
+        livePreview = true,
       })
     end,
-  },
-  {
-    'folke/tokyonight.nvim',
-    lazy = true,
-  },
-  {
-    'projekt0n/github-nvim-theme',
-    lazy = true,
   },
 }
